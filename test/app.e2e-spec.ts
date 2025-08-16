93ffecec56cc5701d7de3ad840475e35890de07b
Gilson Gabriel <gilson@codemastersolucoes.com>
2025-08-16 20:38:27 -0300

    fix tests fastify

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,25 +3,24 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppExpressModule } from './../src/express/app.module';
-import { AppFastifyModule } from './../src/fastify/app.module';
 
 const adapter = process.env.ADAPTER || 'express';
-const AppModule = adapter === 'fastify' ? AppFastifyModule : AppExpressModule;
-const expectedMessage =
-  adapter === 'fastify'
-    ? 'Hello World from Fastify!'
-    : 'Hello World from Express!';
+const expectedMessage = 'Hello World from Express!';
 
 describe(`AppController (e2e) - ${adapter}`, () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppExpressModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterEach(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
